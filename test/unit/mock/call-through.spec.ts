7b6894b55897b8c3aa1c8dbe00e3bd914a28b5bc
smartcontracts <kelvin@optimism.io>
2021-09-30 06:32:21 -0400

    fix: allow mocks to override reverting functions (#84)

--- test/unit/mock/call-through.spec.ts
+++ test/unit/mock/call-through.spec.ts
@@ -31,6 +31,11 @@ describe('Mock: Call through', () => {
     expect(await mock.count()).to.equal(123);
   });
 
+  it('should be able to override a function that reverts', async () => {
+    mock.doRevert.returns(true);
+    expect(await mock.doRevert()).to.equal(true);
+  });
+
   it('should be able to check function calls', async () => {
     await mock.add(10);
     expect(mock.add).to.be.calledOnceWith(10);
