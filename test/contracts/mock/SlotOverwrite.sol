2b470511ab9b0074bfd8fd149dc89c0da75a5374
mirazu <baby.tiger.dev@gmail.com>
2022-07-02 01:59:35 +0900

    fix: slot-overwrite bug

--- test/contracts/mock/SlotOverwrite.sol
+++ test/contracts/mock/SlotOverwrite.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract Bug {
+contract SlotOverwrite {
   address public myAddress;
   bool public myBool;
   address public newAddress;
--- test/unit/mock/slot-overwrite.spec.ts
+++ test/unit/mock/slot-overwrite.spec.ts
@@ -3,7 +3,7 @@ import { ADDRESS_EXAMPLE, ADDRESS_EXAMPLE2, ADDRESS_EXAMPLE3 } from '@test-utils
 import { Bug, Bug__factory } from '@typechained';
 import { expect } from 'chai';
 
-describe('Bug Fix', () => {
+describe('Mock: Slot Overwrite', () => {
   let bugFactory: MockContractFactory<Bug__factory>;
   let mock: MockContract<Bug>;
 
@@ -16,19 +16,13 @@ describe('Bug Fix', () => {
   });
 
   describe('setVariable', () => {
-    it('should be able to fix slot overwrite on packed variables', async () => {
+    it('should not be able to overwrite slot', async () => {
       await mock.setVariable('myAddress', ADDRESS_EXAMPLE);
       await mock.setVariable('myBool', true);
       await mock.setVariable('newAddress', ADDRESS_EXAMPLE2);
       await mock.setVariable('newAddress2', ADDRESS_EXAMPLE3);
       await mock.setVariable('newBool', false);
       await mock.setVariable('newBool2', true);
-      console.log('____myAddress____', await mock.myAddress());
-      console.log('____myBool____', await mock.myBool());
-      console.log('____newAddress____', await mock.newAddress());
-      console.log('____newAddress2____', await mock.newAddress2());
-      console.log('____newBool____', await mock.newBool());
-      console.log('____newBool2____', await mock.newBool2());
       expect(await mock.myAddress()).to.equal(ADDRESS_EXAMPLE);
       expect(await mock.myBool()).to.equal(true);
       expect(await mock.newAddress()).to.equal(ADDRESS_EXAMPLE2);
